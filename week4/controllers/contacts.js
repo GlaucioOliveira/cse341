bb681ac5860db80dbfd2ae6b01f4a9da3ee916b5
Gláucio Oliveira <glaucio.dev@gmail.com>
2025-01-30 01:44:04 -0300

    adding swager and cors permissions

---
+++ week4/controllers/contacts.js
@@ -0,0 +1,97 @@
+const mongodb = require('../db/client');
+const ObjectId = require('mongodb').ObjectId;
+
+const mongoCollection = () => {
+  return mongodb.getDatabaseClient().db().collection('contacts');
+};
+
+const getAll = async (request, response) => {
+  const result = await mongoCollection().find();
+  result.toArray().then((lists) => {
+    response.setHeader('Content-Type', 'application/json');
+    response.status(200).json(lists);
+  });
+};
+
+const getById = async (request, response) => {
+  const userId = new ObjectId(request.params.id);
+  const result = await mongoCollection().find({ _id: userId });
+  result.toArray().then((lists) => {
+    response.setHeader('Content-Type', 'application/json');
+    response.status(200).json(lists[0]);
+  });
+};
+
+const createContact = async (request, response) => {
+  const contact = {
+    firstName: request.body.firstName,
+    lastName: request.body.lastName,
+    email: request.body.email,
+    favoriteColor: request.body.favoriteColor,
+    birthday: request.body.birthday
+  };
+
+  if (
+    !contact.firstName ||
+    !contact.lastName ||
+    !contact.email ||
+    !contact.favoriteColor ||
+    !contact.birthday
+  ) {
+    response.status(400).json('Please provide all required fields.');
+    return;
+  }
+
+  const dbResponse = await mongoCollection().insertOne(contact);
+  if (dbResponse.acknowledged) {
+    response.status(201).json(dbResponse);
+  } else {
+    response
+      .status(500)
+      .json(dbResponse.error || 'Some error occurred while creating the contact.');
+  }
+};
+
+const updateContact = async (request, response) => {
+  const userId = new ObjectId(request.params.id);
+
+  const contact = {
+    firstName: request.body.firstName,
+    lastName: request.body.lastName,
+    email: request.body.email,
+    favoriteColor: request.body.favoriteColor,
+    birthday: request.body.birthday
+  };
+  const dbResponse = await mongoCollection().replaceOne({ _id: userId }, contact);
+  console.log(dbResponse);
+  if (dbResponse.modifiedCount > 0) {
+    response.status(204).send();
+  } else {
+    response
+      .status(500)
+      .json(dbResponse.error || 'Some error occurred while updating the contact.');
+  }
+};
+
+const deleteContact = async (request, response) => {
+  const userId = new ObjectId(request.params.id);
+
+  const dbResponse = await mongoCollection().deleteOne(
+    {
+      _id: userId
+    },
+    true
+  );
+
+  console.log(dbResponse);
+
+  if (dbResponse.deletedCount > 0) {
+    response.status(200).send();
+  } else {
+    response
+      .status(500)
+      .json(dbResponse.error || 'Some error occurred while deleting the contact.');
+  }
+};
+
+module.exports = { getAll, getById, createContact, updateContact, deleteContact };
